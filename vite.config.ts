300edab1dbbf5983b456f2154dac74910de1fd7c
asati <117782931+asa-soporte@users.noreply.github.com>
2026-08-25 12:10:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './', // <-- Fundamental para GitHub Pages
+    base: '/fluxplay-web/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
